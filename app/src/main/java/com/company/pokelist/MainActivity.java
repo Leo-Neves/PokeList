7b1d0200770d82d3db9bb30d7dd041564e14ba96
Leandro Santana <leo93santana@gmail.com>
2018-11-06 16:15:24 -0300

    Aula 2 com conexão a internet

--- app/src/main/java/com/company/pokelist/MainActivity.java
+++ app/src/main/java/com/company/pokelist/MainActivity.java
@@ -6,6 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
     private Button buttonSincronizar;
     private TextView textoPokemon;
@@ -23,5 +30,29 @@ public class MainActivity extends AppCompatActivity {
                 textoPokemon.setVisibility(View.VISIBLE);
             }
         });
+
+        //O código de conexão com a PokeAPI começa aqui
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://pokeapi.co/api/v2/pokemon/";
+
+        //Preparar uma requisição para a URL da PokeAPI
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //Exibindo a resposta da PokeAPI na tela
+                        textoPokemon.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Caso haja algum problema, exibir mensagem de erro
+                textoPokemon.setText("Ocorreu um erro!");
+            }
+        });
+
+        //Executar a requicição HTTP
+        queue.add(stringRequest);
     }
+
 }
